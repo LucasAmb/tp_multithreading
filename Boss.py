0c910fc25397d88db06bf43329c5825f410f824e
Lucas AMBLARD <mbl3597a@univ-tlse3.fr>
2023-11-24 11:57:36 +0100

    add comments

--- Boss.py
+++ Boss.py
@@ -4,10 +4,10 @@ from Manager import QueueClient
 class Boss(QueueClient):
     def __init__(self):
         super().__init__()
+        #Create tasks and add in the queue
         for t in range(10):
             task = Task(t)
             self.tasks.put(task)
-        print("fini")
             
 if __name__ == "__main__":
     boss=Boss()
--- Minion.py
+++ Minion.py
@@ -5,20 +5,20 @@ class Minion(QueueClient):
         super().__init__()
     
     def run(self):
+        #Get a task in the queue
         taskToBeDone=self.tasks.get()
-        print("passe2")
-        if(taskToBeDone is not None):
-            print("Debut travail sur :", taskToBeDone.identifier)
-            taskToBeDone.work()
-            self.results.put = taskToBeDone.x
-            print(" résultat pour la tâche ", taskToBeDone.identifier, ": \n")
-            print(taskToBeDone.x, "\n")
+        #Does the work of the task
+        print("Debut travail sur :", taskToBeDone.identifier)
+        taskToBeDone.work()
+        #Add the result of the task to the result queue
+        self.results.put = taskToBeDone.x
+        print(" résultat pour la tâche ", taskToBeDone.identifier, ": \n")
+        print(taskToBeDone.x, "\n")
 
 if __name__ == "__main__":
     minion = Minion()
     while(1):
         print(minion.tasks)
         minion.run()
-        print("passe1")
 
 
