8f5b8bdbab561fef43f2b245010dfadf408e03e7
Lucas AMBLARD <mbl3597a@univ-tlse3.fr>
2023-12-04 15:06:16 +0100

    Add JSON

--- Task.py
+++ Task.py
@@ -1,5 +1,6 @@
 import time
 
+import json
 import numpy as np
 
 
@@ -19,3 +20,31 @@ class Task:
         start = time.perf_counter()
         self.x = np.linalg.solve(self.a, self.b)
         self.time = time.perf_counter() - start
+
+    def to_json(self) -> str :
+        data = json.dumps({"id":self.identifier, "size":self.size,
+                            "a":self.a.tolist(), "b":self.b.tolist(), "x":self.x.tolist(), "time": self.time})
+        return data
+
+    @classmethod
+    def from_json(cls, text: str) -> "Task":
+        jsonRecup=json.loads(text)
+        obj = cls(jsonRecup["id"])
+        obj.a = np.array(jsonRecup["a"])
+        obj.b = np.array(jsonRecup["b"])
+        obj.x = np.array(jsonRecup["x"])
+        obj.size = np.array(jsonRecup["size"])
+        obj.time = jsonRecup["time"]
+        return obj
+
+    def __eq__(self, other: "Task") -> bool:
+        if isinstance(other, Task):
+            if np.array_equal(self.a, other.a) and np.array_equal(self.b, other.b) and np.array_equal(self.x, other.x):
+                return True
+        return False
+    
+if __name__ == "__main__":
+    taskCree = Task("0")
+    txt = taskCree.to_json()
+    taskReceive = Task.from_json(txt)
+    print(taskCree == taskReceive)
